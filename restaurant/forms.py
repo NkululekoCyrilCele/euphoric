f22b31fce2a2f8e50a6a5304f0e537b955b0e41a
Nkululeko <nkululeko.cele@gmail.com>
2023-02-04 13:52:45 +0200

    Update forms.py, book.html, bookings.html, urls.py, and views.py

--- restaurant/forms.py
+++ restaurant/forms.py
@@ -1,8 +1,35 @@
 from django import forms
 from .models import Bookings
+from phonenumber_field.widgets import PhoneNumberPrefixWidget
 
 
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Bookings
         fields = '__all__'
+        labels = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email',
+            'phone': 'Phone',
+            'reservation_date': 'Reservation Date',
+            'reservation_slot': 'Reservation Slot'
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control'
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control'
+            }),
+            'phone': PhoneNumberPrefixWidget(),
+            'reservation_date': forms.DateInput(attrs={
+                'class': 'forms-control'
+            }),
+            'reservation_slot': forms.NumberInput(attrs={
+                'class': 'forms-control'
+            }),
+        }
